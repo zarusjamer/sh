dbe673a1a3a8103d526f8594ae32958687fc95a8
zarusjamer <zarusjamer@gmail.com>
2018-05-30 22:46:30 +0300

    3.1.0

--- js/components.js
+++ js/components.js
@@ -1244,7 +1244,7 @@ Vue.component( 'itemSlot', {
   template: `
   <span class="htooltip">
     <span class="htooltip-text">{{summary}}</span>
-    <span class="icon item" :class="[ type.icon(), a.iconAffinity() ]"/>
+    <span class="icon item" :class="[ type.icon(), vm.iconAffinity( a ) ]"/>
   </span>
   `,
   props:[ 'type', 'a' ],
